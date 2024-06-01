0dcbcd09b5f46eb83224bef5669615588f056cab
ronan.virmani <ronan.virmani@ufl.edu>
2024-06-01 14:01:50 -0400

    changed some server stuff

--- backend/server.js
+++ backend/server.js
@@ -10,18 +10,13 @@ const app = express();
 app.use(express.json());
 
 // CORS Configuration
-const allowedOrigins = [process.env.FRONTEND_URL];
-
+const allowedOrigins = [process.env.FRONTEND_URL, 'http://localhost:3000'];
 const corsOptions = {
-    origin: '*', 
-    credentials: true, 
-};
-console.log('allowedOrigins:', allowedOrigins);
-
-app.use(cors({
-    origin: ['http://localhost:3000', "https://splitmate-frontend.onrender.com"],
+    origin: allowedOrigins,
     credentials: true,
-}));
+};
+
+app.use(cors(corsOptions));
 
 // Logging Middleware
 app.use((req, res, next) => {
@@ -34,6 +29,14 @@ const groupRoutes = require('./routes/groupRoutes');
 
 app.use('/api/users', userRoutes);
 app.use('/api/groups', groupRoutes);
+
+app.use(express.static(path.join(__dirname, 'build')));
+
+// The "catchall" handler: for any request that doesn't
+// match one above, send back React's index.html file.
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
 
 // MongoDB Connection
 mongoose.connect(process.env.MONGO_URI)

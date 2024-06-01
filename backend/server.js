59f858d8cda2e1b386cf4b6af58726d38c93b2f2
ronan.virmani <ronan.virmani@ufl.edu>
2024-06-01 14:04:17 -0400

    added path

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,8 @@ require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
+const path = require('path');
+
 
 const app = express();
 

e6d8c75898347cde1f72cc15382cf7da36590808
ronan.virmani <ronan.virmani@ufl.edu>
2024-06-04 22:55:53 -0400

    readded direction in landing page

--- frontend/src/pages/LandingPage.js
+++ frontend/src/pages/LandingPage.js
@@ -2,15 +2,18 @@ import React from 'react'
 import Box from '@mui/material/Box';
 import Highlights from '../components/Highlights';
 import Hero from '../components/Hero';
+import { useAuthContext } from '../hooks/useAuthContext';
 
 
 
 function LandingPage() {
 
-  // if (user){
-  //   //redirect to the dashboard
-  //   window.location.href = '/dashboard';
-  // }
+  const { user } = useAuthContext();
+
+  if (user){
+    //redirect to the dashboard
+    window.location.href = '/dashboard';
+  }
 
   return (
     <Box sx={{ bgcolor: 'background.default' }}>
